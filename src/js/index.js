0a2612819cd34b40fe0e4221cc32691618f7f54d
JONATHAN SILVA <silva.jon86@gmail.com>
2016-03-29 14:42:36 -0400

    Initial Commit - finished HW

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+// Javascript Entry Point
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app'
+
+ReactDOM.render (
+<App/>
+, document.querySelector('.app')
+	);
